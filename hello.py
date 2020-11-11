bfb758ce3791e28bfc1fa445326eaca58e7e2f1f
Suraj Wate <suraj.wate@gmail.com>
2020-11-11 08:01:50 +0530

    routing

--- hello.py
+++ hello.py
@@ -3,5 +3,9 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    return 'Index Page'
+
+@app.route('/hello/<name>')
+def hello(name):
+    return 'Hello ' + name 
